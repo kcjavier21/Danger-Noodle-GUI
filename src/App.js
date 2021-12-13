e52612d20b570a93da3f587ad41116f0a1e1ee8f
Ken Javier <kencarlo21@gmail.com>
2021-12-14 02:48:49 +0800

    Line counter added

--- src/App.js
+++ src/App.js
@@ -8,81 +8,55 @@ import loading from "./images/loading.svg";
 class App extends Component {
   state = {
     code: "",
+    numberOfLines: 1,
+    scroll: 0,
     lexicalAnalyzer: [],
-    lexicalError: "",
-    loadingVisibility: false
+    lexicalError: [],
+    loadingVisibility: false,
   };
 
   handleChange = (e) => {
     let code = { ...this.state.code };
     code = e.currentTarget.value;
+    
+    let lines = code.split(/\r|\r\n|\n/);
+    // console.log(lines.length);
 
-    this.setState({ code });
+    this.setState({ code, numberOfLines: lines.length });
   };
 
   submitCode = async (e) => {
     e.preventDefault();
 
     this.setState({ loadingVisibility: true });
-    
-    let code = this.state.code;
-    // code = `head
-    //     var numb x = 0, y = 0, product = 0; 
-    //   spit(||Enter the first number: ||); 
-    //   swallow(x); 
-    
-    //   spit(||Enter the second number: ||); 
-    //   swallow(y); 
-      
-    //   product = multiply(x, y); 
-    //   spit(||The product is: ||, product); 
-    // tail 
-    
-    // numb action multiply(numb n1, numb n2) { 
-    //   return n1 * n2; 
-    // }`;
-    console.log(typeof code);
-    const apiEndpoint = "http://localhost:5000/playground/lexical-analyzer/";
-    let result = await http.post(apiEndpoint, code);
-    console.log(result.data);
 
-    if (result.data.indexOf("Cannot") >= 0) {
-      this.setState({ 
+    let code = this.state.code;
+    const apiEndpoint = "http://localhost:5000/playground/lexical-analyzer/";
+    let result; 
+    
+    try {
+      result = await http.post(apiEndpoint, code);
+    }
+    catch (ex) {
+      // console.log(ex);
+
+      this.setState({
         lexicalAnalyzer: [],
-        lexicalError: result.data, 
-        loadingVisibility: false 
+        lexicalError: ["Internal Server Error occured!"],
+        loadingVisibility: false
       });
+
       return 0;
     }
     
-
-    let objData = result.data.replace(/'/g, `"`);
-    console.log(objData);
-
-    objData = objData.replace(": ", ":");
-    objData = objData.replace(/,/g, ", ");
-    //console.log(objData);
-    objData = objData.replace(/}{/g, `}, {`);
-    //console.log(objData);
-    objData = objData.split("}, {");
-    objData.pop();
-    //console.log(objData);
-
-    let newObjData = [];
-
-    objData.map((item, index) =>
-      index !== 0
-        ? newObjData.push("{" + item + "}")
-        : newObjData.push(item + "}")
-    );
-    //console.log(newObjData);
-
-    let finalObjData = [];
-    // newObjData.map((item) => console.log(JSON.parse(item)));
-    newObjData.map((item) => finalObjData.push(JSON.parse(item)));
-
-    console.log(finalObjData);
-    this.setState({ lexicalAnalyzer: finalObjData, loadingVisibility: false });
+    let objData = result.data;
+    // console.log(objData);
+    
+    this.setState({
+      lexicalAnalyzer: objData.tokens,
+      lexicalError: objData.errors,
+      loadingVisibility: false,
+    });
   };
 
   renderLexicalTable = () => {
@@ -100,7 +74,7 @@ class App extends Component {
             return (
               <tr key={index}>
                 <th scope="row">{index + 1}</th>
-                <td>{item.lexeme}</td>
+                <td>{item.lexeme.replace("\\\\", "\\")}</td>
                 <td>{item.token}</td>
               </tr>
             );
@@ -109,40 +83,81 @@ class App extends Component {
       </table>
     );
   };
+  
 
   render() {
+    let theCodeEditor = React.createRef();
+    let theLineNumbers = React.createRef();
+    let scroll = 0;
+
+    
+
+    
+
+    const viewScrollProgress = () => {
+      try {
+        scroll = theCodeEditor.current.scrollTop;
+        this.setState({ scroll });
+        // console.log(theLineNumbers.current.scrollTop);
+        theLineNumbers.current.scrollTop = scroll;
+      } catch (ex) {
+        // console.log('Catch.')
+      }
+    }
+    
     return (
       <div className="App">
-        <img src={logo} alt="Danger Noodle Logo"/>
+        <img src={logo} alt="Danger Noodle Logo" />
         <span>DangerNoodle</span>
-        <span className="loading"><img style={{visibility: this.state.loadingVisibility ? 'visible' : 'hidden'}} src={loading} alt="Danger Noodle Logo"/></span>
+        <span className="loading">
+          <img
+            style={{
+              visibility: this.state.loadingVisibility ? "visible" : "hidden",
+            }}
+            src={loading}
+            alt="Danger Noodle Logo"
+          />
+        </span>
 
         <div className="container">
           <div className="left-side">
             <form onSubmit={this.submitCode}>
-              <textarea
-                value={this.state.code}
-                onChange={this.handleChange}
-                onKeyDown={e => {
-                  if ( e.key === 'Tab' && !e.shiftKey ) {
-                    document.execCommand('insertText', false, "\t");
-                    e.preventDefault();
-                  }
-                }}
-              ></textarea>
+
+              <div className="code-editor">
+                <div className="line-numbers" ref={theLineNumbers} onScroll={viewScrollProgress}>
+                  {[...Array(this.state.numberOfLines)].map((item, index) => <>{`${index+1}`}<br/></>)}
+                </div>
+
+                <textarea
+                  ref={theCodeEditor}
+                  onScroll={viewScrollProgress}
+                  value={this.state.code}
+                  onChange={this.handleChange}
+                  onKeyDown={(e) => {
+                    if (e.key === "Tab" && !e.shiftKey) {
+                      document.execCommand("insertText", false, "\t");
+                      e.preventDefault();
+                    }
+                  }}
+                />
+              </div>
+              
+              
               <br />
-              <input type="submit" id="submit-btn" value="Submit" className="btn btn-primary"/>
+              <input
+                type="submit"
+                id="submit-btn"
+                value="Submit"
+                className="btn btn-primary"
+              />
             </form>
 
             <div className="error-displayer">
-              <p>{this.state.lexicalError}</p>
+              {this.state.lexicalError.map((item, index) =><p key={index}>{item}</p>)}
             </div>
           </div>
 
-          <div className="right-side">
-            { this.renderLexicalTable() }
-          </div>
-          
+          <div className="right-side">{this.renderLexicalTable()}</div>
         </div>
       </div>
     );
